709af992fc552cced7120b319bb7bbf729d4d2f3
gantalakshmiishwarya85 <135019861+gantalakshmiishwarya85@users.noreply.github.com>
2024-01-08 11:59:45 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -4,9 +4,9 @@ st.header("Types of Dogs")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("American")
-  st.image("./American.jpg",caption="American Dog",width=100,use_column_width=True)
+  st.image("./American.jpeg",caption="American Dog",width=100,use_column_width=True)
   st.write("American dogs are lovely")
   with col2:
   st.subheader("Pomerian Dog")
-  st.image("./Pomerian.jpg",caption="Pomerian Dog",width=100,use_column_width=True)
+  st.image("./Pomerian.jpeg",caption="Pomerian Dog",width=100,use_column_width=True)
   st.write("Pomerian Dogs are cute")
